7cf2ad1d68530b05a10a3f0162a04c032de8e89d
Mayank-Tripathi <minkut2005@gmail.com>
2025-11-11 11:42:38 +0530

    f

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import React from "react";
-import { Button } from "./ui/Button"; // relative path
+import { Button } from "./ui/button"; // relative path
 import { Link } from "react-router-dom"; // if you’re using React Router
 const API = import.meta.env.VITE_API_BASE_URL;
 
